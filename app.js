e3ae7af4bafaa4aa14604a7f075305687c11690b
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-16 15:46:55 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1012,7 +1012,7 @@ class App {
                 this.own_score.innerHTML = this.fox_score;
                 this.score_time = elapsedTime;
                 if (this.all_player_data.length >= 1)
-                    console.log('mylerp', this.all_player_data[0].mesh.position.y);
+                    console.log('>4', this.all_player_data[0].mesh.position.y);
                 // let data = {
                 //     title: 'score_update',
                 //     id: this.myID,
@@ -1395,7 +1395,7 @@ class App {
                     this.all_player_data[j].plane_type = -1;
 
                 }
-                if (this.all_player_data[j].mesh.position.y > 22) {
+                if (Math.abs(this.all_player_data[j].final_positiony - this.all_player_data[j].mesh.position.y) > 4) {
                     //this.all_player_data[j].mesh.position.y -= 4.2;
                     // if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
                     //     this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 4.2 / test);
