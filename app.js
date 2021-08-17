2366af43ef8a3af6b496b1842063b7781b92c2b8
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-16 21:20:23 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1012,7 +1012,7 @@ class App {
                 this.own_score.innerHTML = this.fox_score;
                 this.score_time = elapsedTime;
                 if (this.all_player_data.length >= 1)
-                    console.log('wtff', this.all_player_data[0].mesh.position.y);
+                    console.log('4.2', this.all_player_data[0].mesh.position.y);
                 // let data = {
                 //     title: 'score_update',
                 //     id: this.myID,
@@ -1397,14 +1397,14 @@ class App {
                     }
                 }
                 else {
-                    //this.all_player_data[j].mesh.position.y -= 0.15;
-                    if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
-                        this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
+                    this.all_player_data[j].mesh.position.y -= 0.15;
+                    // if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
+                    //     this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
                     this.all_player_data[j].onplane = null;
                     this.all_player_data[j].plane_type = -1;
 
                 }
-                if (Math.abs(this.all_player_data[j].final_positiony - this.all_player_data[j].mesh.position.y) > 4) {
+                if (Math.abs(this.all_player_data[j].final_positiony - this.all_player_data[j].mesh.position.y) >= 4.2) {
                     //this.all_player_data[j].mesh.position.y -= 4.2;
                     // if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
                     //     this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 4.2 / test);
