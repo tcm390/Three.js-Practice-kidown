1dcc1ada1beaea0217c5b30b0399c8019724b893
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-16 22:01:20 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1012,7 +1012,7 @@ class App {
                 this.own_score.innerHTML = this.fox_score;
                 this.score_time = elapsedTime;
                 if (this.all_player_data.length >= 1)
-                    console.log('finalx', this.all_player_data[0].mesh.position.y);
+                    console.log('spring', this.all_player_data[0].mesh.position.y);
                 // let data = {
                 //     title: 'score_update',
                 //     id: this.myID,
@@ -1404,19 +1404,21 @@ class App {
                     this.all_player_data[j].plane_type = -1;
 
                 }
-                if (this.all_player_data[j].mesh.position.y > 22) {
-                    this.all_player_data[j].mesh.position.y -= 4.2;
-                    this.all_player_data[j].onplane = null;
-                    this.all_player_data[j].plane_type = -1;
-                }
-                else if (Math.abs(this.all_player_data[j].final_positiony - this.all_player_data[j].mesh.position.y) >= 4.5) {
-                    //this.all_player_data[j].mesh.position.y -= 4.2;
-                    // if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
-                    //     this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 4.2 / test);
+                if (this.all_player_data[j].mesh.position.y > 22
+                    || Math.abs(this.all_player_data[j].final_positiony - this.all_player_data[j].mesh.position.y) >= 4.5
+                ) {
                     this.all_player_data[j].mesh.position.y = this.all_player_data[j].final_positiony;
                     this.all_player_data[j].onplane = null;
                     this.all_player_data[j].plane_type = -1;
                 }
+                // else if (Math.abs(this.all_player_data[j].final_positiony - this.all_player_data[j].mesh.position.y) >= 4.5) {
+                //     //this.all_player_data[j].mesh.position.y -= 4.2;
+                //     // if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
+                //     //     this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 4.2 / test);
+                //     this.all_player_data[j].mesh.position.y = this.all_player_data[j].final_positiony;
+                //     this.all_player_data[j].onplane = null;
+                //     this.all_player_data[j].plane_type = -1;
+                // }
 
                 if (Math.abs(this.all_player_data[j].final_positionx - this.all_player_data[j].mesh.position.x) >= 5) {
                     this.all_player_data[j].mesh.position.x = this.all_player_data[j].final_positionx;
