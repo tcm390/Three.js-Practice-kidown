a2fb2867f06c1ef3c0eafd51903d095f35ae0ed5
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-16 20:55:13 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1012,7 +1012,7 @@ class App {
                 this.own_score.innerHTML = this.fox_score;
                 this.score_time = elapsedTime;
                 if (this.all_player_data.length >= 1)
-                    console.log('uu', this.all_player_data[0].mesh.position.y);
+                    console.log('wtf', this.all_player_data[0].mesh.position.y);
                 // let data = {
                 //     title: 'score_update',
                 //     id: this.myID,
@@ -1355,12 +1355,16 @@ class App {
                             //this.all_player_data[j].mesh.position.y -= 0.15;
                             if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
                                 this.all_player_data[j].mesh.position.y = this.myLerp(this.all_player_data[j].mesh.position.y, this.all_player_data[j].final_positiony, 0.15 / test);
+                            this.all_player_data[j].onplane = null;
+                            this.all_player_data[j].plane_type = -1;
                             // this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
                         }
                         else {
                             if (elapsedTime - this.all_player_data[j].onplane_time > 0.1) {
                                 if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
                                     this.all_player_data[j].mesh.position.y = this.myLerp(this.all_player_data[j].mesh.position.y, this.all_player_data[j].final_positiony, 0.15 / test);
+                                this.all_player_data[j].onplane = null;
+                                this.all_player_data[j].plane_type = -1;
                                 // this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
                                 //this.all_player_data[j].mesh.position.y -= 0.15;
                                 this.all_player_data[j].onplane.rotation.x += Math.PI / 10;
@@ -1376,6 +1380,8 @@ class App {
                         if ((this.all_player_data[j].mesh.position.y - this.all_player_data[j].onplane.position.y - 1) >= 0.4) {
                             if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
                                 this.all_player_data[j].mesh.position.y = this.myLerp(this.all_player_data[j].mesh.position.y, this.all_player_data[j].final_positiony, 0.15 / test);
+                            this.all_player_data[j].onplane = null;
+                            this.all_player_data[j].plane_type = -1;
                             // this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
                             //this.all_player_data[j].mesh.position.y -= 0.15;
 
@@ -1388,9 +1394,9 @@ class App {
                     }
                 }
                 else {
-                    this.all_player_data[j].mesh.position.y -= 0.15;
-                    // if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
-                    //     this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
+                    //this.all_player_data[j].mesh.position.y -= 0.15;
+                    if (this.all_player_data[j].final_positiony < this.all_player_data[j].mesh.position.y)
+                        this.all_player_data[j].mesh.position.lerp(new THREE.Vector3(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positiony, 0), 0.15 / test)
                     this.all_player_data[j].onplane = null;
                     this.all_player_data[j].plane_type = -1;
 
