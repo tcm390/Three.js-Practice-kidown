852a8e5dbcf5a1db0f3487df7a883644bef886ac
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-17 23:23:04 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1425,7 +1425,7 @@ class App {
                     this.all_player_data[j].plane_type = -1;
                 }
 
-                if (new THREE.Vector3(this.all_player_data[j].final_positionx, this.all_player_data[j].final_positiony, 0).distanceTo(this.all_player_data[j].mesh.position) > 20) {
+                if (new THREE.Vector3(this.all_player_data[j].final_positionx, this.all_player_data[j].final_positiony, 0).distanceTo(this.all_player_data[j].mesh.position) > 10) {
                     this.all_player_data[j].mesh.position.y = this.all_player_data[j].final_positiony;
                     this.all_player_data[j].mesh.position.x = this.all_player_data[j].final_positionx;
                     this.all_player_data[j].onplane = null;
