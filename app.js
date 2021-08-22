5ad2108b797c2864566d31519171568f6f89099e
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-21 21:03:19 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1810,7 +1810,7 @@ class App {
                         //     this.all_player_data[j].onplane = null;
                         //     this.all_player_data[j].plane_type = -1;
                         // }
-                        if (Math.abs(this.all_player_data[j].final_positiony[1] - this.all_player_data[j].mesh.position.y) >= 4.2) {
+                        if (this.all_player_data[j].mesh.position.y - this.all_player_data[j].final_positiony[1] >= 4.2) {
                             let test = (this.all_player_data[j].mesh.position.y - this.all_player_data[j].final_positiony[1]);
                             this.all_player_data[j].mesh.position.y = this.myLerp(this.all_player_data[j].mesh.position.y, this.all_player_data[j].final_positiony[1], 4.2 / test);
                             //this.all_player_data[j].mesh.position.y = this.all_player_data[j].final_positiony[1];
