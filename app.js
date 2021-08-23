cfa6e9243b24eba09146c71baa3a5fab748ddaca
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-23 14:11:02 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1637,7 +1637,7 @@ class App {
                         //     this.all_player_data[j].mesh.position.x = this.myBezier(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positionx[0], this.all_player_data[j].final_positionx[1], 0.25 / test);
                         // }
                         // else
-                        if (test >= 0.25) {
+                        if (test > 0.25) {
                             this.all_player_data[j].mesh.position.x = this.myLerp(this.all_player_data[j].mesh.position.x, this.all_player_data[j].final_positionx[1], 0.25 / test);
                         }
                         else {
