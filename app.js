f3b7fe187180f99384d75090f4e8453651cc3731
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-23 18:59:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1800,7 +1800,7 @@ class App {
                                     //     this.all_player_data[j].plane_type = -1;
                                     // }
                                     // else {
-                                    this.all_player_data[j].mesh.position.y = this.all_player_data[j].onplane.position.y + 1;
+                                    // this.all_player_data[j].mesh.position.y = this.all_player_data[j].onplane.position.y + 1;
                                     // }
                                 }
                             }
