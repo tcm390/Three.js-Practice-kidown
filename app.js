189f66074c106fc116d480ff382147272557d3ea
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-23 19:17:48 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1765,7 +1765,7 @@ class App {
                                     this.all_player_data[j].onplane = null;
                                     this.all_player_data[j].plane_type = -1;
                                 }
-                                else if (elapsedTime - this.all_player_data[j].onplane_time > 0.1) {
+                                else if (elapsedTime - this.all_player_data[j].onplane_time > 0.05) {
                                     this.all_player_data[j].onplane.rotation.x += Math.PI / 10;
                                     // this.all_player_data[j].onplane = null;
                                     // this.all_player_data[j].plane_type = -1;
