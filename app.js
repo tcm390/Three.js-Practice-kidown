b7d41e0121db52b0f5121b013f84a44c5281c285
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-24 12:44:14 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1855,7 +1855,7 @@ class App {
                                     this.all_player_data[j].mesh.position.y -= 0.15;
 
                                 }
-                                if (this.all_player_data[j].mesh.position.y < this.all_player_data[j].onplane.position.y + 1.4) {
+                                if (this.all_player_data[j].mesh.position.y < this.all_player_data[j].onplane.position.y + 1) {
                                     if (this.all_player_data[j].onplane.rotation.x === 0)
                                         this.all_player_data[j].onplane.rotation.x += Math.PI / 10;
                                     this.all_player_data[j].onplane = null;
