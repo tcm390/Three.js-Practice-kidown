58abd46bff673b3ff665ff04d15758fabadfacb3
tcm390 <60634884+tcm390@users.noreply.github.com>
2021-08-25 14:27:13 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2046,12 +2046,12 @@ class App {
                     //     }
                     // }
                     if (this.fox
-                        && this.all_player_data[j].mesh.position.distanceTo(this.fox.position) <= 3.7
-                        && Math.abs(this.fox.position.y - this.all_player_data[j].mesh.position.y) <= 2
-                        // && this.all_player_data[j].onplane
-                        // && this.fox_plane) {
-                    ) {
-                        if (this.all_player_data[j].mesh.position.x > this.fox_finalpositionx) {
+                        && Math.abs(this.fox.final_positionx - this.all_player_data[j].mesh.final_positionx[1]) <= 2
+                        && Math.abs(this.fox.final_positiony - this.all_player_data[j].mesh.final_positiony[1]) <= 2
+                        && this.all_player_data[j].onplane
+                        && this.fox_plane) {
+
+                        if (this.all_player_data[j].mesh.final_positionx[1] > this.fox_finalpositionx) {
                             this.all_player_data[j].mesh.position.x += 0.25;
                             // if (this.all_player_data[j].animation === 1)
                             //     this.fox.position.x -= 0.5;
@@ -2060,7 +2060,7 @@ class App {
                             this.fox.position.x -= 0.25
                         }
                         else {
-                            this.all_player_data[j].mesh.position.x -= 0.25;
+                            this.all_player_data[j].mesh.final_positionx[1] -= 0.25;
                             // if (this.all_player_data[j].animation === 2)
                             //     this.fox.position.x += 0.5;
                             // else
